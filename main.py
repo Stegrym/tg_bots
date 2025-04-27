17ed306ea8bd9b700dcd7abbadeb5b01e2438f2f
Stanislav Semerikov <stassemerikov@gmail.com>
2025-04-27 15:34:44 +0300

    27.04.25

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 import telebot
+import os
+
+bot = telebot.TeleBot(os.environ['TOKEN'])
+
+
+@bot.message_handler(commands=["start"])
+def start_message(message: telebot.types.Message):
+    bot.send_message(message.chat.id, "START!")
+
+
+bot.infinity_polling()
