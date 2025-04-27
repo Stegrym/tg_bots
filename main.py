53ec5d067b743acdceb63e8897b51207a116f785
Stanislav Semerikov <stassemerikov@gmail.com>
2025-04-27 19:36:20 +0300

    27.04.25

--- main.py
+++ main.py
@@ -6,7 +6,14 @@ bot = telebot.TeleBot(os.environ['TOKEN'])
 
 @bot.message_handler(commands=["start"])
 def start_message(message: telebot.types.Message):
-    bot.send_message(message.chat.id, "START!")
+    # parse_mode="html" - даёт возможность форматировать текст на языке html
+    bot.send_message(message.from_user.id, "<b>START!</b>", parse_mode="html")
+
+
+@bot.message_handler()
+def info(message: telebot.types.Message):
+    if message.text.lower() == "id":
+        bot.reply_to(message, f"{message.from_user.id}")
 
 
 bot.infinity_polling()
